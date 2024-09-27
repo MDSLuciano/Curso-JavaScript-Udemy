dd9b0679ac7d986ba1345f017b4dfc278688f42d
Luciano Machado <lucianoo_machado@hotmail.com>
2024-09-27 14:13:38 -0300

    Fazendo a pagina de login e validando os formularios atraves do csrf

---
+++ JavaScriptAvancado/ProjetoAgenda/routes.js
@@ -0,0 +1,17 @@
+//Iremos fazer aqui o tratamento das rotas, 
+//e importando as funções do nosso controller.
+const express = require('express')
+const route = express.Router()
+const homeController = require('./src/controllers/homeController')
+const loginController = require('./src/controllers/loginController')
+
+
+// Rotas da home
+route.get('/', homeController.index);
+
+//Rotas de login
+route.get('/login/index', loginController.index)
+
+
+module.exports = route
+
