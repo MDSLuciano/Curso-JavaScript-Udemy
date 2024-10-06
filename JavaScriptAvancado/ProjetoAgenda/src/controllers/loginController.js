0fbe9acae6652b22908fee7a88ca24a82c3d2459
Luciano Machado <lucianoo_machado@hotmail.com>
2024-10-06 14:23:56 -0300

    Cadastrando Contato

--- JavaScriptAvancado/ProjetoAgenda/src/controllers/loginController.js
+++ JavaScriptAvancado/ProjetoAgenda/src/controllers/loginController.js
@@ -1,26 +1,54 @@
 const Login = require('../models/LoginModel')
 exports.index = (req, res) => {
-    res.render('login')
+  res.render('login')
 }
 
 //Essa funcionalidade vai registrar o usuário, mas precisa ser validada,
 //por isso teremos que fazer esse metódo seja async também
 exports.register = async function (req, res) {
-    try{
+  try {
     const login = new Login(req.body)
     await login.register()
-  
+
     if (login.errors.length > 0) {
       req.flash('errors', login.errors) // Adiciona as mensagens de erro à sessão
       res.req.session.save(() => res.redirect('/login/index')) // Redireciona para a página de login
       return
     }
-  
+
     req.flash('sucess', 'Seu usuário foi criado com sucesso') // Adiciona as mensagens de erro à sessão
-    return res.req.session.save(() => res.redirect('/login/index')) // Redireciona para a página de login
-    }catch(e){
-        console.log(e);
-        return res.render('404')
-    }
-    
+    res.req.session.save(() => res.redirect('/login/index')) // Redireciona para a página de login
+  } catch (e) {
+    console.log(e);
+    return res.render('404')
   }
+
+}
+
+exports.login = async function (req, res) {
+  try {
+    const login = new Login(req.body)
+    await login.login()
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors) // Adiciona as mensagens de erro à sessão
+      res.req.session.save(() => res.redirect('/login/index')) // Redireciona para a página de login
+      return
+    }
+
+
+    req.flash('sucess', 'Você entrou no sistema.') // Adiciona as mensagens de erro à sessão
+    //Isso permite que o usuário não precise ficar logando sempre.
+    req.session.user = login.user
+    res.req.session.save(() => res.redirect('/')) // Redireciona para a página de login
+  } catch (e) {
+    console.log(e);
+    return res.render('404')
+  }
+
+}
+
+exports.logout = function(req, res) {
+  req.session.destroy()
+  res.redirect('/')
+}
--- JavaScriptAvancado/ProjetoAgenda/src/models/LoginModel.js
+++ JavaScriptAvancado/ProjetoAgenda/src/models/LoginModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const validator = require('validator')//biblioteca para validar os dados
 const bcryptjs = require('bcryptjs')//biblioteca para criptografar os dados
 
-const LoginSchema = new mongoose.Schema({     //Criando o Schema do mongoose para fazer os tratamentos dos dados
+//Criando o Schema do mongoose para fazer os tratamentos dos dados
+const LoginSchema = new mongoose.Schema({ 
     email: { type:String, required: true},
     password: { type:String, required: true}
 })
@@ -17,6 +18,25 @@ class Login {
         this.user = null
     }
 
+    async login(){
+        this.valida()
+        if(this.errors.length > 0) return
+        this.user = await LoginModel.findOne({email: this.body.email})
+        
+        if(!this.user) {
+            this.errors.push('Usuário não existe')
+            return
+        }
+
+        if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+            this.errors.push('Senha inválida')
+            this.user = null
+            return
+        }
+    }
+
+
+
 //Essa função vai registrar o usuário, mas para isso ela irá esperar o
 //método de validação.
     async register() {
@@ -30,17 +50,15 @@ class Login {
         const salt = bcryptjs.genSaltSync()//Gerando o hash da senha
         this.body.password = bcryptjs.hashSync(this.body.password, salt)//Criptografando a senha
 
-        try{
-            this.user = await LoginModel.create(this.body)
-        }catch(e) {
-            console.log(e)
-        }
+
+        this.user = await LoginModel.create(this.body)
     }
     
     async userExists() {
         this.user = await LoginModel.findOne({email: this.body.email})
         if(this.user) this.errors.push('E-mail ja existe')
     }
+
     valida(){
         this.cleanUp()
         // Validação
