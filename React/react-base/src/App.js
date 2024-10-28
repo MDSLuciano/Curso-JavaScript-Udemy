075556548bdfcc039b6521918c00d90173bb0b7a
Luciano Machado <lucianoomachados@gmail.com>
2024-10-28 06:38:08 -0300

    Configurando um projeto base react

---
+++ React/react-base/src/App.js
@@ -0,0 +1,13 @@
+import Login from "./pages/login";
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <Login />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
