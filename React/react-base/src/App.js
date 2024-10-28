e01965dd25bee50a30848dda757764ba57ed0b4e
Luciano Machado <lucianoomachados@gmail.com>
2024-10-28 11:53:40 -0300

    React Router Dom v5 aprendendo o básico

--- React/react-base/src/App.js
+++ React/react-base/src/App.js
@@ -1,14 +1,17 @@
-import Login from "./pages/login";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
 import GlobalStyles from "./styles/GlobalStyles";
 import Header from "./components/Header";
+import Routes from "./routes";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+    </ BrowserRouter>
   );
 }
 
